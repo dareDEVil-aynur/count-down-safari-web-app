4ed3c7058412ea1c706cdc4a08487f4842627599
Aynur Rahman Talukdar <128217234+dareDEVil-aynur@users.noreply.github.com>
2024-02-19 18:16:56 +0200

    make new year target date

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import CountdownTimer from "./CountdownTimer";
 
 function App() {
   /* change this date according to your needs */
-  const targetDate = "2024-03-04T00:00:00";
+  const targetDate = "2025-01-01T00:00:00";
 
   return (
     <div className="App">
